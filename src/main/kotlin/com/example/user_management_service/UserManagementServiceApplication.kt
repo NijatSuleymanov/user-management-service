430abfec6b0d9efbf7d330f516017ba0f52d2a92
Nicat <snicat894@gmail.com>
2025-11-18 11:52:59 +0400

    Initial project structure

---
+++ src/main/kotlin/com/example/user_management_service/UserManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.user_management_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserManagementServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserManagementServiceApplication>(*args)
+}
